18272ebfa25396d7fc1aa70132a53411478caf46
Omer Yassir <omerym10@gmail.com>
2024-12-31 20:41:26 +0200

    sfdgsg

--- Part2_Q4.cpp
+++ Part2_Q4.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
 #include <algorithm>
 #include <climits>
+#include <vector>
 #include "Prim.cpp"
 using namespace std;
+struct Edge
+{
+    int x;
+    int y; 
+    int g;
+    int s;
+};
+
 int main()
 {
     int cityCount, roadCount;
     cin >> cityCount >> roadCount;
     int goldPrice, silverPrice;
     cin >> goldPrice >> silverPrice;
+    vector<Edge> edges;
     for (int i = 0; i < roadCount; i++)
     {
-        int x, y, g, s;
-        cin >> x >> y >> g >> s;
-        x--;
-        y--;
+        Edge edge;
+        cin >> edge.x >> edge.y >> edge.g >> edge.s;
+        edge.x--;
+        edge.y--;
     }
 }
